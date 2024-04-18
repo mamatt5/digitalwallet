2e15b3c53095eb44f4b17df05c15fb230707a8af
Jenny <43059884+IkarosJenny@users.noreply.github.com>
2024-04-18 11:17:25 +1000

    dataload

--- Backend/app/dataloader.py
+++ Backend/app/dataloader.py
@@ -1,1 +1,30 @@
 # Faker
+
+from faker import Faker
+from models.merchant import Merchant
+from models.user import User
+
+fake = Faker()
+
+def generate_dummy_data(num_records):
+    data = []
+    for _ in range(num_records):
+        account_data = {
+            "email": fake.email(),
+            "password": fake.password()
+        }
+        if fake.boolean():
+            account_data.update({
+                "company_name": fake.company(),
+                "ABN": fake.msisdn()  
+            })
+            account = Merchant(**account_data)
+        else:
+            account_data.update({
+                "first_name": fake.first_name(),
+                "last_name": fake.last_name()
+            })
+            account = User(**account_data)
+        
+        data.append(account)
+    return data
--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,6 +1,8 @@
 from database import init_db
 from routes import merchant_route, user_route
 from fastapi import FastAPI
+from dataloader import generate_dummy_data
+from sqlmodel import create_engine, Session
 
 app = FastAPI()
 
@@ -8,7 +10,14 @@ app = FastAPI()
 @app.on_event("startup")
 def on_startup():
     init_db()
+    dummy_data = generate_dummy_data(10)
+
+    engine = create_engine("sqlite:///database.db")
+    with Session(engine) as session:
+        session.add_all(dummy_data)
+        session.commit()
 
 
 app.include_router(user_route.router)
 app.include_router(merchant_route.router)
+
