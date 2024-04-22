2e1788083f94e3ac5ac541abfc59e34a72659282
Lachlan Vaughan <lachlan.vaughan@fdmgroup.com>
2024-04-22 15:02:52 +1000

    Fixed issue with main.py lifecycle

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,14 +1,12 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
 from database import get_db_session, init_db
 from dataloader import generate_dummy_data, insert_dummy_data
-from fastapi import FastAPI
 from routes import auth_route, merchant_route, user_route
 
-# Creates a FastAPI instance
-app = FastAPI()
 
-
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     """
     Startup event handler
 
@@ -19,19 +17,23 @@ def on_startup():
     On shutdown:
     - Handles resource cleanup, closes database connection
     """
-    
+
     # Startup
     init_db()
 
-    # Generate and insert dummy data
     num_records = 10
     dummy_data = generate_dummy_data(num_records)
     with next(get_db_session()) as session:
         insert_dummy_data(session, dummy_data)
-    
+
     # Application execution
     yield
 
+    # Shutdown
+    pass
+
+# Creates a FastAPI instance
+app = FastAPI(lifespan=lifespan)
 
 # Include the routes/endpoints for the app
 app.include_router(user_route.router)
