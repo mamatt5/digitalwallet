8e5aca966c9087431dff178bbfc24d9de64d1bcd
Augustine Mai <167040753+AugustineMai@users.noreply.github.com>
2024-05-27 12:08:53 +1000

    Upload Profile

---
+++ Frontend/navigation/AuthNavigator.tsx
@@ -0,0 +1,35 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import LoginScreen from '../pages/Login/LoginScreen';
+import RegisterUserScreen from '../pages/Register/RegisterUserScreen';
+import ForgotPasswordScreen from '../pages/ForgotPassword/ForgotPasswordScreen';
+import RegisterOptionScreen from '../pages/Register/RegisterOptionScreen';
+import RegisterMerchantScreen from '../pages/Register/RegisterMerchantScreen';
+import ResetPasswordScreen from '../pages/ForgotPassword/ResetPasswordScreen';
+import ReceiptsScreen from '../pages/Receipts/ReceiptsScreen';
+
+const AuthStack = createStackNavigator();
+
+function AuthNavigator() {
+  return (
+    <AuthStack.Navigator
+      screenOptions={{
+        headerTitle: ' ',
+        headerStyle: {
+          backgroundColor: '#0f003f',
+        },
+        headerTintColor: '#fff',
+      }}
+    >
+      <AuthStack.Screen name="Login" component={LoginScreen} />
+      <AuthStack.Screen name="RegisterUser" component={RegisterUserScreen} />
+      <AuthStack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+      <AuthStack.Screen name="RegisterOption" component={RegisterOptionScreen} />
+      <AuthStack.Screen name="RegisterMerchant" component={RegisterMerchantScreen} />
+      <AuthStack.Screen name="ResetPassword" component={ResetPasswordScreen} />
+      <AuthStack.Screen name="Receipts" component={ReceiptsScreen} />
+    </AuthStack.Navigator>
+  );
+}
+
+export default AuthNavigator;
