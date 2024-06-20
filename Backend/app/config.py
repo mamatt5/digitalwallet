c1a708d4a33d81017611136e5eb74d337f79cfc1
Ben <shuyan.liu1@wsu.edu>
2024-06-20 10:44:43 +1000

    Revert startup.bat to start everything

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# DEV_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'dev_database.db')}"
-DEV_DATABASE_URL = "mysql://admin1:password-1@ap-plus1.mysql.database.azure.com:3306/ap_plus"
+DEV_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'dev_database.db')}"
+# DEV_DATABASE_URL = "mysql://admin1:password-1@ap-plus1.mysql.database.azure.com:3306/ap_plus"
 TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
 
 JWT_ALGORITHM = "HS256"
