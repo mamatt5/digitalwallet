a449d74cab4d89c8848dfb4c05228df42743ea17
Jeremie Brogniez <jeremie.brogniez@fdmgroup.com>
2024-06-21 06:25:04 +0000

    Jeremie develop

--- PayPath_Plugin_Frontend/src/Pages/PayPathQRDisplay.jsx
+++ PayPath_Plugin_Frontend/src/Pages/PayPathQRDisplay.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import QRCode from 'qrcode.react';
+import { QRCodeSVG } from 'qrcode.react';
 import axios from 'axios';
 
 const PayPathQRDisplay = () => {
 
     const [paymentData, setPaymentData] = useState(null);
+    // const [account, setAccount] = useState(null)
+
+    const [updatedData, setData] = useState({})
+
+
     const ENDPOINT = "http://localhost:8000/transactions/gettransactions";
     const [numTransactions, setNumTransactions] = useState(0)
     const [paymentSuccess, setPaymentSuccess] = useState(false)
@@ -36,8 +41,11 @@ const PayPathQRDisplay = () => {
         const intervalId = setInterval(() => {
             getTransactions().then(
                 response => {
+                    console.log(numTransactions)
                     if (response.length > numTransactions) {
                         setPaymentSuccess(true)
+                        console.log("Success!!")
+                        console.log(response.length)
                         window.parent.postMessage("Success", "http://localhost:5173");
                     }
                 }
@@ -47,7 +55,7 @@ const PayPathQRDisplay = () => {
         }, 2000);
 
         return () => clearInterval(intervalId);
-    }, []);
+    }, [numTransactions]);
 
     useEffect(() => {
 
@@ -58,30 +66,88 @@ const PayPathQRDisplay = () => {
             console.log('Received message:', event.data);
 
             setPaymentData(event.data);
+
+            setData({
+                items: event.data.items,
+                account_id: event.data.vendor,
+                wallet_id: event.data.vendor,
+                merchant: event.data.merchant_name,
+                amount: event.data.amount,
+                description: event.data.description,
+            })
         };
 
         window.addEventListener("message", handleMessage);
+
+        
 
         return () => {
             window.removeEventListener("message", handleMessage);
         };
     }, [])
+  
+    // const data = [
+    //     {
+    //         name: "Wool Blend Wrap Felt Coat in Camel",
+    //         price: 109.98,
+    //         quantity: 1
+    //     },
+    //     {
+    //         name: "Double Breasted Cut Away Chrombie Coat in Grey",
+    //         price: 99.98,
+    //         quantity: 1
+    //     }
+    // ]
 
+    const calculateTotalPrice = (data) => {
+        let total = 0;
+        for (let item of data) {
+            total += item.price
+        }
+        return total
+    }
+
+    // console.log(updatedData)
+
+    // const qrData = 
+  
+
+  
     return (
-        <div>
-            <h1>PayPath Payment</h1>
-            {paymentData && (
-                <div>
-                    <h2>Payment Details</h2>
-                    <p>Items: {paymentData.items.join(", ")}</p>
-                    <p>Description: {paymentData.description}</p>
-                    <p>Amount: ${paymentData.amount}</p>
-                    <p>Vendor: {paymentData.vendor}</p>
-                    <QRCode value={JSON.stringify(paymentData)} />
-                </div>
+    <div style={{"display":"flex", "flexDirection":"column"}}>
+        <h1>Pay with PayPath</h1>
+        <div style={{"display":"flex", "flexDirection":"column"}}>
+            {paymentData ? (
+                <>
+                    <div style={{"display":"flex", "flexDirection":"column", "paddingBottom":"60px"}}>
+                        <h2>{paymentData.merchant_name}</h2>
+                        <h2>Payment Details</h2>
+                        <div>Items: 
+                            {paymentData.items.map((item) => (
+                                <p>{item.name}</p>
+                            ))
+                        }</div>
+                    </div>
+                    <div>
+                        <p>Total: ${updatedData.amount}</p>
+                        <p>Scan this QR code in your PayPath app to confirm:</p>
+                        <QRCodeSVG
+                            value={JSON.stringify(updatedData)}
+                            size={260}
+                        />
+                    </div>
+                </>
+            ) : (
+                <p>Waiting for payment data...</p>
             )}
         </div>
-    )
+        {/* <div>
+            <p>Don't have the app yet? Install here:</p>
+            <button>Get PayPath on your mobile</button>
+        </div> */}
+    </div>
+  )
+
 }
 
 export default PayPathQRDisplay
