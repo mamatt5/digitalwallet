2424beeb50431bb41133ea5f0117776049523683
Ben <shuyan.liu1@wsu.edu>
2024-06-25 11:23:57 +1000

    Add QR Image Storage

---
+++ Backend/app/routes/qr_image_route.py
@@ -0,0 +1,41 @@
+from models.qr_image import QRImage
+from services.qr_image_service import QRImageService
+from fastapi import APIRouter, File, UploadFile, HTTPException, Depends
+from fastapi.responses import StreamingResponse
+from typing import Dict
+import io
+
+router = APIRouter(prefix="/qr_images", tags=["QRImages"])
+
+
+@router.post("/add")
+async def upload_image_route(name: str, merchant_id: int, file: UploadFile = File(...),  qr_image_service: QRImageService = Depends(QRImageService)) -> Dict:
+    try:
+        file_data = await file.read()
+        image = qr_image_service.create_image(name=name, data=file_data, merchant_id=merchant_id)
+        return {"id": image.qrimage_id, "name": image.name, "merchant_id": image.merchant_id}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Image upload failed") from e
+
+
+@router.get("/get/{qr_image_id}")
+def get_qr_image_route(qr_image_id: int, qr_image_service: QRImageService = Depends(QRImageService)) -> QRImage:
+    image = qr_image_service.get_qr_image(qr_image_id)
+    if image is None:
+        raise HTTPException(status_code=404, detail="Image not found")
+    return StreamingResponse(io.BytesIO(image.data), media_type="image/png")
+
+
+@router.put("/update")
+async def update_qr_image_route(name: str, merchant_id: int, file: UploadFile = File(...),  qr_image_service: QRImageService = Depends(QRImageService)) -> Dict:
+    try:
+        file_data = await file.read()
+        image = qr_image_service.create_image(name=name, data=file_data, merchant_id=merchant_id)
+        return {"id": image.qrimage_id, "name": image.name, "merchant_id": image.merchant_id}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Image upload failed") from e
+
+
+@router.delete("/delete/{qr_image_id}")
+def delete_qr_image_route(qr_image_id: int, qr_image_service: QRImageService = Depends(QRImageService)) -> None:
+    qr_image_service.delete_qr_image(qr_image_id)
