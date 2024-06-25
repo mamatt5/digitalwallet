64b5dcc79ca51aee759283fed4dbd9ddad51b4a1
Sarah L <sarah.li@fdmgroup.com>
2024-06-25 16:28:02 +1000

    Added route for landing page

---
+++ AnalyticsWebsite/src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Landing = () => {
+
+    return (
+      <div>
+        <h1>APPLUS</h1>
+      </div>
+    );
+  };
+  
+  export default Landing;
