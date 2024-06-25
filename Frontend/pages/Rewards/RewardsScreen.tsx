1718fb59a6d5904820dee9834b329984f2a173b5
jefferson <jefferson.zheng@fdmgroup.com>
2024-06-25 11:12:53 +1000

    added basic page for rewards

--- Frontend/pages/Rewards/RewardsScreen.tsx
+++ Frontend/pages/Rewards/RewardsScreen.tsx
@@ -1,200 +1,109 @@
 import React, { useEffect, useState } from "react";
 import {
   SafeAreaView,
-  ScrollView,
   View,
   Text,
+  Dimensions,
   StyleSheet,
-  Image,
-  Pressable,
+  Alert,
 } from "react-native";
-import MaterialIcon from "react-native-vector-icons/MaterialCommunityIcons";
-import DetailedItemCard from "../../components/DetailedItemCard/DetailedItemCard";
-import { getItems, getMerchant, getCard, getAPPoints } from "../../api/api";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import { TextInput } from "react-native-gesture-handler";
-import LoyaltyRewardCard from "../../components/LoyaltyRewardCard/LoyaltyRewardCard";
 
-function RewardsScreen({ navigation, route }) {
-  const [refresh, setRefresh] = useState(false);
+import RewardTabs from "../../components/CardFilterTabs/RewardTabs";
 
-  useEffect(() => {
-    const unsubscribe = navigation.addListener("focus", () => {
-      setRefresh((prev) => !prev);
-    });
+const { width, height } = Dimensions.get("window");
+const scale = width / 320;
 
-    return unsubscribe;
-  }, [navigation]);
+function RewardsScreen({navigation, route}) {
 
-  const { account } = route.params;
-  const [walletPoints, setWalletPoints] = useState(0);
+    const [activeTabIndex, setActiveTabIndex] = useState(0);
 
-    const Row = ({ children }) => (
-        <View style={styles.row}>{children}</View>
-      )
 
-    const Col = ({ children }) => {
-    return  (
-        <View style={styles.col}>{children}</View>
-    )
-    }
 
-    const fetchWalletPoints = async () => {
-      try {
-        const response = await getAPPoints(account.wallet.wallet_id);
-        setWalletPoints(response);
-      } catch (error) {
-        console.error("Get Wallet Points error:", error);
-      }
-    }
-
-    useEffect(() => {
-      fetchWalletPoints();
-    }, [refresh]);
-
-  return (
+    return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Pressable
-          style={styles.backButtonContainer}
-        >
-          <MaterialIcon name="chevron-left" size={50} color="#FFFFFF" />
-        </Pressable>
-        <Text style={styles.transactionDate}>
-            Choose a reward
-        </Text>
-      </View>
-        <View style={styles.textInputContainer}>
-          <Ionicons style={styles.textInputIcon}name="search" size={15} color="#FFFFFF" />
-          <TextInput style={styles.textInput} placeholder="Search" placeholderTextColor="white" />
+      <View style={styles.centerView}>
+        <View style={styles.header}>
+          <Text style={styles.titleText}>Rewards</Text>
+          <View style={styles.profileButton}>
+          </View>
         </View>
-        <View style={styles.loyaltyCardContainer}>
-        <Text style={styles.transactionDate}>Points: {walletPoints}</Text>
-        <ScrollView style={styles.rewardCardContainer}>
-      <Row>
-        <Col>
-            <LoyaltyRewardCard itemDetails={{rewardPrice: 500, rewardNumber: 1}}></LoyaltyRewardCard>
-        </Col>
-        <Col>
-            <LoyaltyRewardCard itemDetails={{rewardPrice: 1000, rewardNumber: 1}}></LoyaltyRewardCard>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <LoyaltyRewardCard itemDetails={{rewardPrice: 2000, rewardNumber: 1}}></LoyaltyRewardCard>
-        </Col>
-        <Col>
-        <LoyaltyRewardCard itemDetails={{rewardPrice: 500, rewardNumber: 2}}></LoyaltyRewardCard>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <LoyaltyRewardCard  itemDetails={{rewardPrice: 1000, rewardNumber: 2}}></LoyaltyRewardCard>
-        </Col>
-        <Col>
-        <LoyaltyRewardCard  itemDetails={{rewardPrice: 2000, rewardNumber: 2}}></LoyaltyRewardCard>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <LoyaltyRewardCard itemDetails={{rewardPrice: 500, rewardNumber: 3}}></LoyaltyRewardCard>
-        </Col>
-        <Col>
-        <LoyaltyRewardCard  itemDetails={{rewardPrice: 1000, rewardNumber: 3}}></LoyaltyRewardCard>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <LoyaltyRewardCard  itemDetails={{rewardPrice: 2000, rewardNumber: 3}}></LoyaltyRewardCard>
-        </Col>
-        <Col>
-        <LoyaltyRewardCard  itemDetails={{rewardPrice: 500, rewardNumber: 4}}></LoyaltyRewardCard>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <LoyaltyRewardCard  itemDetails={{rewardPrice: 1000, rewardNumber: 4}}></LoyaltyRewardCard>
-        </Col>
-        <Col>
-        <LoyaltyRewardCard  itemDetails={{rewardPrice: 2000, rewardNumber: 4}}></LoyaltyRewardCard>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <LoyaltyRewardCard  itemDetails={{rewardPrice: 500, rewardNumber: 5}}></LoyaltyRewardCard>
-        </Col>
-        <Col>
-        <LoyaltyRewardCard  itemDetails={{rewardPrice: 1000, rewardNumber: 5}}></LoyaltyRewardCard>
-        </Col>
-      </Row>
-    </ScrollView>
+        
+        <RewardTabs
+          activeTabIndex={activeTabIndex}
+          setActiveTabIndex={setActiveTabIndex}
+        />
+
         </View>
-    </SafeAreaView>
-  );
+        </SafeAreaView>
+    );
 }
 
+
 const styles = StyleSheet.create({
+  centerView: {
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+    marginTop: 30,
+  },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  profileButton: {
+    marginLeft: 10 * scale,
+  },
   container: {
     backgroundColor: "#0f003f",
-    flex: 1
+    flex: 1,
   },
-  headerContainer: {
-    marginHorizontal: 15,
+  iconContainer: {
     marginTop: 10,
-    display: "flex",
-    flexDirection: "row",
+  },
+  noCardContainer: {
     alignItems: "center",
+    borderColor: "#fff",
+    borderRadius: 10,
+    borderStyle: "dashed",
+    borderWidth: 2,
+    height: 150,
     justifyContent: "center",
+    width: 250,
   },
-  backButtonContainer: {
-    width: 50,
-    position: "absolute",
-    left: 0,
-  },
-  ShareButtonContainer: {
-    width: 50,
-    position: "absolute",
-    right: 0,
-  },
-  transactionDate: {
-    color: "#FFFFFF",
+  noCardText: {
+    color: "#fff",
     fontSize: 20,
   },
-  textInputContainer: {
+  noTransactionsContainer: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+  titleText: {
+    color: "#ffffff",
+    fontSize: 20 * scale,
+    fontWeight: "bold",
+    marginVertical: 10,
+  },
+  pointsContainer: {
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    borderColor: 'white',
-    borderRadius: 100,
-    borderWidth: 2,
-    width: '85%',
-    margin: 15,
-    height: 30
   },
-  textInputIcon: {
-    paddingLeft: 10
-  },
-    textInput: {
-    color: 'white',
+  pointsText: {
+    color: "#ffffff",
+    fontSize: 20 * scale,
+    fontWeight: "bold",
     marginLeft: 10,
-    height: 30,
-    width: '100%'
   },
-  loyaltyCardContainer: {
-    marginHorizontal: 15
+  transactionContainer: {
+    flex: 1,
+    paddingHorizontal: 10 * scale,
   },
-  rewardCardContainer: {
-    marginHorizontal: "auto",
-    width: '100%', 
-    height: 500
-    },
-  row: {
-    flexDirection: "row"
+  transactions: {
+    flex: 1,
   },
-  col:  {
-    flex:  1,
-    margin: 5
-  }
 });
 
 export default RewardsScreen;
